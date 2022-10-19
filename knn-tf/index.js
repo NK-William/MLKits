97adfd227095bd04450eda142dfb2e3b585a113c
Nkuna William <64930245+NK-William@users.noreply.github.com>
2022-10-19 14:05:45 +0200

    Added KNN algorithm

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -5,6 +5,20 @@ const tf = require('@tensorflow/tfjs'); // well here it will use GPU by defaule 
 // I commented require('@tensorflow/tfjs-node'); because it was giving me errors
 const loadCSV = require('./load-csv');
 
+function knn(features, labels, predictionPoint, k) {
+return features
+    .sub(predictionPoint)
+    .pow(2)
+    .sum(1)
+    .pow(.5) // hypotenuse now complete 
+    .expandDims(1)
+    .concat(labels, 1)
+    .unstack()  // separates a tensor by rows of tensors inside a JS array
+    .sort((a, b) => a.get(0) > b.get(0)? 1 : -1) // using a JS operation since we have JS array of tensors
+    .slice(0, k) // JS version of slice
+    .reduce((acc, pair) => acc + pair.get(1), 0) / k // JS version of reduce
+}
+
 let { features, labels, testFeatures, testLabels} = loadCSV('kc_house_data.csv', {
     shuffle: true,
     splitTest: 10, // this will set aside 10 set of data for testing purposes.
@@ -12,5 +26,11 @@ let { features, labels, testFeatures, testLabels} = loadCSV('kc_house_data.csv',
     labelColumns: ['price']
 })
 
-console.log(testFeatures)
-console.log(testLabels)
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+
+// const result = knn(features, labels, tf.tensor(testFeatures[0]), 10);
+// console.log('Guess', result, testLabels[0][0]);
+
+const ts = tf.tensor([1,2,3,4,5,6]);
+console.log("value: ", ts.get(0));
